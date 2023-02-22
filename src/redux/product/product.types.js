4892948d0c061e23eadb459572863e801c1b1a6e
RevatiJunghare <jungharerevati804@gmail.com>
2023-02-22 09:05:45 +0530

    revati_day2

---
+++ src/redux/product/product.types.js
@@ -0,0 +1,5 @@
+export const GET_PRODUCTS_SUCCESS = "products/get/success"
+export const GET_PRODUCTS_LOADING = "products/get/loading"
+export const GET_PRODUCTS_ERROR = "products/get/error"
+
+export const GET_EARBUD_PRODUCTS_SUCCESS = "earbudproducts/get/success"
