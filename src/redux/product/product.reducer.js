47c33b0073e3e08f5c42a2c24db2a4d72a741543
RevatiJunghare <jungharerevati804@gmail.com>
2023-02-23 09:09:30 +0530

    revati_day3

--- src/redux/product/product.reducer.js
+++ src/redux/product/product.reducer.js
@@ -1,10 +1,11 @@
-import {GET_PRODUCTS_SUCCESS,GET_PRODUCTS_LOADING,GET_PRODUCTS_ERROR,GET_EARBUD_PRODUCTS_SUCCESS} from "./product.types"
+import {GET_PRODUCTS_SUCCESS,GET_PRODUCTS_LOADING,GET_PRODUCTS_ERROR,GET_EARBUD_PRODUCTS_SUCCESS,GET_FILTERED_PRODUCTS, SORT_projectorES, SORT_projectorESS, SORT_alphabet} from "./product.types"
 
 const initialState = {
     loading : false,
     error : false,
     data : [],
-    product : {}
+    product : {},
+    filteredBrandData:[]
 }
 
 export const productReducer = (state=initialState,{type,payload})=>{
@@ -31,6 +32,35 @@ export const productReducer = (state=initialState,{type,payload})=>{
         error : false,
         data : payload
     }
+    case SORT_projectorES: {
+        if (payload == "high") {
+          let sorted = state.data.sort(
+            (a, b) => Number(a.price) - Number(b.price)
+          );
+  
+          return { ...state, data: [...sorted] };
+        } else if (payload == "low") {
+          let sorted = state.data.sort(
+            (a, b) => Number(b.price) - Number(a.price)
+          );
+          return { ...state, data: [...sorted] };
+        }
+      };
+break
+      case SORT_alphabet: {
+        if (payload == "z") {
+          let sorted = state.data.sort(
+            (a, b) => (a.title) - (b.title)
+          );
+  
+          return { ...state, data: [...sorted] };
+        } else if (payload == "a") {
+          let sorted = state.data.sort(
+            (a, b) => b.title - a.title
+          );
+          return { ...state, data: [...sorted] };
+        }
+      }
     default:return state
    }
 }
