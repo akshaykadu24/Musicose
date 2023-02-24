2804b5d3b251d17a7a0bc663083cb9cd7393d6a1
My Name <aakarshpatel84@gmail.com>
2023-02-24 22:05:17 +0530

    Add

--- src/pages/earbuds/Earbuds.jsx
+++ src/pages/earbuds/Earbuds.jsx
@@ -1,11 +1,11 @@
 import {
   Box,
-  // Drawer,
-  // DrawerBody,
-  // DrawerCloseButton,
-  // DrawerContent,
-  // DrawerHeader,
-  // DrawerOverlay,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   Text,
   useDisclosure,
   Grid,
@@ -13,17 +13,41 @@ import {
   Image,
   HStack,
   Tag,
+  Button,
+  Center,
+  Wrap,
+  Heading,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { StarIcon } from "@chakra-ui/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { getearbudproduct } from "../../redux/product/product.action";
 import { BsFillFilterSquareFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import ProductHeader from "../../components/Revati_components/ProductHeader";
+import SideBar from "../../components/Revati_components/SideBar";
+import axios from "axios";
+import Products_box from "../../components/Revati_components/Products_box";
 
 const EarbudProducts = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [placement, setPlacement] = useState("left");
+  const [search, setSearch] = useState("");
+  const [slider, setSlider] = useState(0);
+  const low = Number((slider[0] * 3199) / 100);
+  const high = Number((slider[1] * 3199) / 100);
+  const [sort, setSort] = useState("");
+  const [popularity, setPopularity] = useState("");
+  const [alphabet, setAlphabet] = useState("");
+  const [ascending, setAscending] = useState("");
+  const [descending, setDescending] = useState("");
+  const [alphasort, setAlphasort] = useState([]);
+  const navigate = useNavigate();
+
+  if (sort == "pop-desc") {
+    setSort("");
+    setPopularity("desc");
+  }
 
   const dispatch = useDispatch();
   const products = useSelector((store) => {
@@ -70,137 +94,45 @@ const EarbudProducts = () => {
 
   return (
     <>
-      <Box
-        display={{ base: "block", lg: "flex" }}
-        p="10px"
-        position="relative"
-        style={{ border: "1px solid red" }}
-      >
-        {/* <Box
-          flex={0.27}
-          display={{ base: "none", lg: "block" }}
-          style={{ border: "1px solid black", height: "450px" }}>
+      <Center py={12} backgroundColor={"rgb(245,245,245)"}>
+        <Wrap spacing={"80px"} width={"90%"} justify="center">
+          <ProductHeader
+            products={products}
+            search={search}
+            setSearch={setSearch}
+            sort={sort}
+            setSort={setSort}
+            popularity={popularity}
+            setPopularity={setPopularity}
+            ascending={ascending}
+            setAscending={setAscending}
+            descending={descending}
+            setDescending={setDescending}
+          />
+
           <Box
-            pl="12px"
-            display={{ base: "flex", lg: "none" }}
-            alignItems="center">
-            <BsFillFilterSquareFill onClick={onOpen} fontSize={20} color="#E40046"/>
-            <Text onClick={onOpen} cursor="pointer" fontSize="20px" fontWeight="bold" ml="7px" color={"black"}>
-              Filter
-            </Text>
-            <Drawer  placement={placement}  onClose={onClose} isOpen={isOpen}>
-              <DrawerOverlay />
-              <DrawerContent>
-                <DrawerCloseButton />
-                <DrawerHeader borderBottomWidth="1px">Filter</DrawerHeader>
-                <DrawerBody>
-                  
-
-
-
-      
-
-      <Wrap spacing={"80px"} width={"90%"} justify="center">
-
-                </DrawerBody>
-              </DrawerContent>
-            </Drawer>
-          </Box>
-        </Box> */}
-
-        <Box flex={1} p="15px 15px" style={{ border: "1px solid red" }}>
-          {products.data.length === 0 && (
-            <Box>
-              <Image
-                width="600px"
-                h="500px"
-                m="auto"
-                src="https://media.tenor.com/unvXyxtdn3oAAAAC/no-result.gif"
-              />
-            </Box>
-          )}
-
-          <Grid
-            templateColumns={{
+            w={{ base: "75%", md: "85%", lg: "95%" }}
+            display={"grid"}
+            gridTemplateColumns={{
               base: "repeat(1,1fr)",
               sm: "repeat(2,1fr)",
               md: "repeat(3,1fr)",
               lg: "repeat(3,1fr)",
-              xl: "repeat(4,1fr)",
             }}
-            rowGap="20px"
-            mt="15px"
+            gap={"15px"}
+            m={"auto"}
+            h={"auto"}
+            style={{ marginTop: "-50px" }}
           >
-            {products.data?.map((item) => {
-              return (
-                <Link to={`/products/${item.id}`}>
-                  <GridItem
-                    m="auto"
-                    p="15px 7px"
-                    minW="210px"
-                    maxWidth="260px"
-                    key={item.id}
-                    position="relative"
-                    className="product-card"
-                  >
-                    <Image
-                      m="auto"
-                      src={item.images}
-                      width="200px"
-                      height="230px"
-                      alt="image"
-                    />
-                    <Text color="#748A9C" fontSize="14px" mt={2}>
-                      {item.subtitle}
-                    </Text>
-                    <Text color="#748A9C" fontSize="14px">
-                      {item.title}
-                    </Text>
-                    <HStack
-                      display="flex"
-                      h="30px"
-                      alignItems="center"
-                      flexWrap="wrap"
-                    >
-                      <Text
-                        fontSize="13px"
-                        color="#D6D0DB"
-                        textDecoration="line-through"
-                      >
-                        Rs. {item.strike_price}
-                      </Text>
-                      <b fontSize="13px">Rs. {item.price}</b>
-
-                      <Tag size="xs" fontSize="12px" colorScheme="gray">
-                        {item.discount}
-                      </Tag>
-                    </HStack>
-
-                    <Box display="flex" alignItems="center">
-                      {Array(5)
-                        .fill("")
-                        .map((e, i) => (
-                          <StarIcon
-                            boxSize={3}
-                            key={i}
-                            color={
-                              i < Math.floor(item.rating)
-                                ? "yellow.400"
-                                : "gray.300"
-                            }
-                          />
-                        ))}
-                      <Box as="span" ml="2" color="#D6D0DB" fontSize="sm">
-                        ({item.rating})
-                      </Box>
-                    </Box>
-                  </GridItem>
+            {products.data.length > 0 &&
+              products.data?.map((e, i) => (
+                <Link to={`/products/${e.id}`}>
+                  <Products_box e={e} />
                 </Link>
-              );
-            })}
-          </Grid>
-        </Box>
-      </Box>
+              ))}
+          </Box>
+        </Wrap>
+      </Center>
     </>
   );
 };
