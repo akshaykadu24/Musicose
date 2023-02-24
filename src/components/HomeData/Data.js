be5194cd2879cf232a22bfa18c2a8fca30a407e3
My Name <aakarshpatel84@gmail.com>
2023-02-24 21:50:07 +0530

    Add responsive

---
+++ src/components/HomeData/Data.js
@@ -0,0 +1,162 @@
+export const Daily_deals = [
+    {
+        id: 1,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/c2386af9-4349-432f-8ba5-2b6aa06025c8_400x.png?v=1642405569",
+        pname: "boAt Airdopes 131 - Wireless Earbuds",
+        price: "899",
+        strprice: " 2,990",
+        rating: " 4.8 | 1186 reviews",
+        dec: "You Save: ₹ 2091 (70%)",
+    },
+    {
+        id: 2,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/Main1_38d12adf-3a49-46be-9a91-3c9f75431353_500x.png?v=1654195093",
+        pname: "boAt Rockerz 260",
+        price: "999",
+        strprice: " 2,499",
+        rating: " 4.9 | 1007 reviews",
+        dec: "You Save: ₹ 1500 (60%)",
+    },
+    {
+        id: 3,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main_blue_759e7ad4-18af-4407-9e8a-91c0058b1a74_600x.png?v=1648108782",
+        pname: "Wave Lite Smartwatch - 1.69 ...",
+        price: "1299",
+        strprice: " 6,990",
+        rating: " 4.9 | 919 reviews",
+        dec: "You Save: ₹ 5691 (81%)",
+    },
+    {
+        id: 4,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/6af12e30-e6d0-43d3-afd2-500065309a3e_400x.png?v=1625552657",
+        pname: "boAt Rockerz 255 Pro+",
+        price: "1299",
+        strprice: " 3,990",
+        rating: " 4.8 | 309 reviews",
+        dec: "You Save: ₹ 2691 (67%)",
+    },
+    {
+        id: 5,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/1_eebd7966-a481-44a9-91a2-bef06d4c0f05_400x.png?v=1663821109",
+        pname: "boAt Airdopes 402 - Wireless Earbudss",
+        price: "1699",
+        strprice: " 5,990",
+        rating: " 4.8 | 932 reviews",
+        dec: "You Save: ₹ 4291 (72%)",
+    },
+    {
+        id: 6,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/103blk_400x.png?v=1574927262",
+        pname: "boAt BassHeads 103",
+        price: "369",
+        strprice: " 1,290",
+        rating: " 4.9 | 982 reviews",
+        dec: "You Save: ₹ 2791 (70%)",
+    },
+    {
+        id: 7,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/7d635fe4-8c4c-4c4b-8c00-6065455dc608_400x.png?v=1625046386",
+        pname: "boAt BassHeads 242",
+        price: "449",
+        strprice: " 1,490",
+        rating: " 4.8 | 840 reviews",
+        dec: "You Save: ₹ 1041 (70%)",
+    },
+    {
+        id: 8,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/336d66bb-60e6-4b02-831b-864ea4fada9e_400x.png?v=1625045731",
+        pname: "boAt Airdopes 381 - In Ear Wireless Earbuds",
+        price: "1599",
+        strprice: " 4,990",
+        rating: " 4.8 | 432 reviews",
+        dec: "You Save: ₹ 3391 (67%)",
+    },
+    {
+        id: 71,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/main-image-2_400x.png?v=1655534170",
+        title: "Stone 190 DC Edition",
+        rating: "5.0 | 102 reviews",
+        strprice: "2990",
+        price: "1299",
+        dec: "You Save: ₹ 1691 (57%)",
+    },
+    {
+        id: 73,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/03_4ac3cfb6-1075-4376-9dc1-51bdf79b8558_400x.jpg?v=1656100876",
+        title: "Airdopes 131-Wonder",
+        rating: "4.8 | 156 reviews",
+        strprice: "7,990",
+        price: "2999",
+        dec: "You Save: ₹ 4991 (62%)",
+    },
+    {
+        id: 1,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/2_cae7e95b-ed02-4b49-9a88-e4e5324111ba_400x.png?v=1655888228",
+        title: "boAt Rockerz 460",
+        rating: "5.0 | 2 reviews",
+        strprice: "2,990",
+        price: "1299",
+        dec: "You Save: ₹ 1691 (57%)",
+    },
+    {
+        id: 3,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/7499af44-9d7a-489c-80f0-51f7799289e4_400x.png?v=1625045491",
+        title: "boAt Rockerz 450",
+        rating: "4.8 | 121 reviews",
+        strprice: "7,990",
+        price: "2999",
+        dec: "You Save: ₹ 4991 (62%)",
+    },
+    {
+        id: 1,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/402-main-img_400x.png?v=1630059242",
+        title: "boAt Airdopes 402-Wireless",
+        rating: "4.8 | 166 reviews",
+        strprice: "3,490",
+        price: "1299",
+        dec: "You Save: ₹ 2191 (63%)",
+    },
+    {
+        id: 2,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/airdopes-121-v2-blue_400x.png?v=1612765376",
+        title: "boAt Airdopes 121 v2",
+        rating: "4.9 | 476 reviews",
+        strprice: "3,990",
+        price: "1399",
+        dec: "You Save: ₹ 2591 (65%)",
+    },
+    {
+        id: 81,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/450ca_400x.png?v=1639990534",
+        title: "boAt Rockerz 450-Captain",
+        rating: "4.8 | 126 reviews",
+        strprice: "7,990",
+        price: "2999",
+        dec: "You Save: ₹ 4991 (62%)",
+    },
+    {
+        id: 83,
+        image:
+            "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/190ca_400x.png?v=1639989015",
+        title: "boAt Stone 190-Captain",
+        rating: "4.9 | 146 reviews",
+        strprice: "3,990",
+        price: "1399",
+        dec: "You Save: ₹ 2591 (65%)",
+    },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Carousel from "../components/Carousel";
-import Header from "../components/Header";
+import { Dailydeals } from "../components/Dailydeals";
+// import Header from "../components/Header";
+// import { Dailydeals } from "../components/Dailydeals";
 const Home = () => {
   return (
     <div>
       {/* <Header />; */}
       <Carousel />
+      <Dailydeals />
     </div>
   );
 };
