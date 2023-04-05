0ec3186dca646e3adf1459a22238945132695f25
Akshay Kadu <103638977+akshaykadu24@users.noreply.github.com>
2023-04-06 01:35:37 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,10 +9,10 @@ function About() {
         <h1 className="checkoutHead">Musicose</h1>
       </div>
       <div className="content">
-        <h3 className="musicoseAbout">Musicose is an Indian manufacturing company that specializes in audio equipment. They offer an extensive product catalog that includes headphones, earphones, speakers, travel chargers, and premium cables. Imagine Marketing Services Private Limited, operating under the name Musicose, is India's fastest-growing audio and wearables brand. They offer a wide range of high-quality wireless earphones, earbuds, headphones, smartwatches, and home audio products.</h3>
-        <h3 className="musicose">As a part of construct we were assigned to clone a website using React, Node.js, and MongoDB. We our a team of 4 members:</h3>
+        <h3 className="musicoseAbout">Musicose is an Indian Electronic manufacturing company we specializes in audio equipment. we offer an extensive product catalog that includes headphones, earphones, speakers, earbuds and Watches . Musicose is India's fastest-growing audio and wearables brand. we offer a wide range of high-quality wireless earphones, earbuds, headphones, smartwatches, and home audio products.</h3>
+        <h3 className="musicoseAbout"> We have done a partnership with the smartwatch brand of owner tony, now on our website smartwatches are also available in premium and affordable price range </h3>
         <ul className="nameList">
-          <li>Akshay Kude (Team Lead)</li>
+          <li >Akshay Kadu (Team Lead)</li>
           <li>Preeti Bildani</li>
           <li>Revati Junghare</li>
           <li>Aakarsh Patel</li>
